0e638bbb9ad6d47d18db4687a64c91efe2841474
Gary Chung <garychung@ps.asl.com.hk>
2022-02-14 22:05:23 +0800

    add StudentConfig and StudentRepository

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student gary = new Student("Gary", "gary6480@gmail.com", LocalDate.of(1992, Month.MARCH, 4), 29);
+
+            Student wendy = new Student("Wendy", "wendyNg@gmail.com", LocalDate.of(1992, Month.MARCH, 18), 29);
+
+            repository.saveAll(
+                    List.of(gary, wendy)
+            );
+        };
+    }
+
+
+}
